a8bcd8993bac360e77c02ba71dc7c21cf1f5e512
tlin108 <tlin108@hotmail.com>
2017-02-28 00:00:05 -0500

    refactor with redux form

--- src/routes/register/Register.js
+++ src/routes/register/Register.js
@@ -6,12 +6,26 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
-
+import _ from 'lodash';
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import { reduxForm } from 'redux-form';
 import s from './Register.css';
 
-import { UsernameField, PasswordField } from '../../components/Auth';
+const FIELDS = {
+  username: {
+    type: 'input',
+    label: 'Username:',
+  },
+  password: {
+    type: 'password',
+    label: 'Password:',
+  },
+  confirmPassword: {
+    type: 'password',
+    label: 'Confirm Password:',
+  },
+};
 
 class Register extends React.Component {
   static propTypes = {
@@ -61,43 +75,26 @@ class Register extends React.Component {
     return username.length >= 5 && password.length >= 8 && password === confirmPassword;
   }
 
+  renderField = fieldConfig => (
+    <div key={fieldConfig.label} className={s.formGroup}>
+      <label className={s.label} htmlFor={fieldConfig.label}>
+        {fieldConfig.label}
+      </label>
+      <input
+        type={fieldConfig.type}
+        id={fieldConfig.label}
+        className={s.input}
+      />
+    </div>
+    )
+
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
           <h1>{this.props.title}</h1>
           <form method="post">
-            <div className={s.formGroup}>
-              <label className={s.label} htmlFor="username">
-                Username:
-              </label>
-              <UsernameField
-                className={s.input}
-                onUsernameChange={this.onUsernameChange}
-              />
-              <small className="form-text text-muted">Minimum 5 characters</small>
-            </div>
-            <div className={s.formGroup}>
-              <label className={s.label} htmlFor="password">
-                Password:
-              </label>
-              <PasswordField
-                className={s.input}
-                passwordType={'password'}
-                onPasswordChange={this.onPasswordChange}
-              />
-              <small className="form-text text-muted">Minimum 8 characters</small>
-            </div>
-            <div className={s.formGroup}>
-              <label className={s.label} htmlFor="confirmPassword">
-                Confirm Password:
-              </label>
-              <PasswordField
-                className={s.input}
-                passwordType={'confirmPassword'}
-                onPasswordChange={this.onConfirmPasswordChange}
-              />
-            </div>
+            {_.map(FIELDS, this.renderField)}
             <div className={s.formGroup}>
               <button
                 className={s.button}
@@ -114,4 +111,7 @@ class Register extends React.Component {
   }
 }
 
-export default withStyles(s)(Register);
+export default withStyles(s)(reduxForm({
+  form: 'registerUser',
+  fields: _.keys(FIELDS),
+})(Register));
